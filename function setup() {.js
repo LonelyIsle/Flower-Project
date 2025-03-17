d57b6655fad123754463b75a753555873c87da00
William <d.j.stewart7873@gmail.com>
2025-03-17 16:54:37 -0500

    main

--- function setup() {.js
+++ function setup() {.js
@@ -1,16 +1,20 @@
 function setup() {
     createCanvas(400, 400);
-  }
+    console.log("Canvas created!");
+}
   
-  function draw() {
+function draw() {
     background(220);
+    console.log("Drawing...");
     translate(width / 2, height / 2);
+  
     let petals = 6;
     for (let i = 0; i < petals; i++) {
-      push();
-      rotate(TWO_PI * i / petals);
-      fill(255, 0, 100);
-      ellipse(30, 0, 50, 80);
-      pop();
+        push();
+        rotate(TWO_PI * i / petals);
+        fill(255, 0, 100);
+        ellipse(30, 0, 50, 80);
+        console.log("Petal drawn at", i);
+        pop();
     }
-  }
+}
